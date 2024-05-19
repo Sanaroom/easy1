64d8c49db0e0f5445d0bcf3fc44072a626e034d7
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-19 17:31:38 +0900

    コメント　表示機能index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
   root to: "checks#index"
   resources :checks, only: [:index, :new, :create,:show]
+    resources :comments, only: :index
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_19_064238) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_19_082400) do
   create_table "checks", charset: "utf8", force: :cascade do |t|
     t.string "number", default: "", null: false
     t.string "attendance", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
