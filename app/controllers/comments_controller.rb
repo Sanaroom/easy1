c30a3f552d23fefc7787f2fb356d4eb2eec426e4
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-20 11:02:44 +0900

    コメント投稿　保存

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,26 @@
 class CommentsController < ApplicationController
   def index
     @comments=Comment.all
+    @comment=Comment.new
   end
 
   
-  
- 
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      redirect_to root_path
+    else
+      
+      render :index
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:board)
+  end
 
 end
