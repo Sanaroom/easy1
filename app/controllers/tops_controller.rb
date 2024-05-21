7b839eac0f51c806aa9d29f7a7bd7600eb8020aa
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-21 14:49:23 +0900

    top/indexに遷移するとログアウトになる

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,13 @@
 class TopsController < ApplicationController
+  before_action :logout, only: [:index]
 
   def index
+  end
+
+  private
+
+  def logout
+    sign_out current_user if user_signed_in?
   end
   
 end
