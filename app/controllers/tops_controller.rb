554d3d8035f70964d89ff4d733f1ee0db4e6150b
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-05-21 15:01:33 +0900

    top/indexには誰でも遷移可

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,13 +1,20 @@
 class TopsController < ApplicationController
-  before_action :logout, only: [:index]
+  #before_action :login, only: [:index]
+  before_action :move_to_index, except: [:index]
 
   def index
   end
 
   private
 
-  def logout
-    sign_out current_user if user_signed_in?
+  #def logout
+    #sign_out current_user if user_signed_in?
+  #end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
   
 end
