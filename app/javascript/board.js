a4a305b84fbd8342edb49ac23f26b5ea53ecdbf7
Sanaroom <167286039+Sanaroom@users.noreply.github.com>
2024-06-24 10:24:50 +0900

    修正

--- app/javascript/board.js
+++ app/javascript/board.js
@@ -3,7 +3,7 @@ window.addEventListener('turbo:load', function(){
   const board = document.getElementById('commentSelect')
   const changeBoard = document.getElementById('board-text')
 
-  if (this.value !==null){
+  if (board !==null){
     board.addEventListener('change', function() {
     changeBoard.value += this.value;
   });
